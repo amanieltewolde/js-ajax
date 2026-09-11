18e410ae975bafbe93c428d105e190ddb5a2237d
amanieltewolde <aman94@hotmail.it>
2026-09-12 01:41:42 +0200

    chiamata AJAX

--- js/app.js
+++ js/app.js
@@ -1,3 +1,23 @@
 // variabile per identificare endpoint del server
 const endpoint = 'https://flynn.boolean.careers/exercises/api/random/mail';
 
+// chiamata AJAX
+// creazione fx async per ottenere email randomica
+async function getEmail() {
+
+    // variabile per richiesta HTTP
+    const response = await fetch(endpoint);
+    // console.log(response);
+
+    // variabile del dato ottenuto dal server, letto in JS 
+    const data = await response.json();
+    // console.log(data);
+
+    // 
+    console.log(data.response);
+
+
+}
+
+getEmail()
+
